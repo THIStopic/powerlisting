b8eca5ab5cffe7fe463a2a031bf5f55b6fd6019b
RafaGonzalezDev <rafagonzalezdeveloper@gmail.com>
2023-07-14 07:46:50 +0200

    Cambio nombre de la aplicación.

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
     return (
         <div className="App font-poppins w-full md:max-w-screen-xl h-full md:h-screen p-12">
-            <h1 className="text-2xl text-center text-primarytext font-semibold mb-8">TodoTS + Redux</h1>
+            <h1 className="text-2xl text-center text-primarytext font-semibold mb-8">PowerListing</h1>
             <AddTodo />
             <Separator />
             <Filter />
