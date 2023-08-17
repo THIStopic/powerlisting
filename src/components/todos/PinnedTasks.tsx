3f9f0d614211dbc47058eb3f092b10d14c70e153
RafaGonzalezDev <rafagonzalezdeveloper@gmail.com>
2023-08-17 20:27:40 +0200

    Corregidas tarjetas para tareas fijadas.

--- src/components/todos/PinnedTasks.tsx
+++ src/components/todos/PinnedTasks.tsx
@@ -2,33 +2,49 @@
 import { useSelector } from 'react-redux';
 import { useAutoAnimate } from '@formkit/auto-animate/react';
 
+// Custom hooks
+import useTodoActions from '../hooks/useTodoActions'; // Aquí importamos el custom hook useTodoActions.
+
 const PinnedTasks = () => {
     const todos = useSelector((state: any) => state.todos.todos);
     const [parent] = useAutoAnimate();
 
     const pinnedTodos = todos.filter((todo: any) => todo.isPinned);
+    const { togglePin } = useTodoActions();
 
     return (
         <div>
-            {pinnedTodos.length > 0 ? (
-                <div>
-                    <h1 className="mt-8 mb-4 flex justify-start font-medium">Tareas fijadas:</h1>
-                    <ul ref={parent}>
-                        {pinnedTodos.map((todo: any) => (
-                            <li key={todo.id}>{todo.title}</li>
-                        ))}
-                    </ul>
-                </div>
-            ) : (
-                <div>
-                    <h1 className="mt-8 mb-4 flex justify-center font-medium">¡No hay tareas fijadas!</h1>
-                    <ul ref={parent} className='hidden'>
-                        {pinnedTodos.map((todo: any) => (
-                            <li key={todo.id}>{todo.title}</li>
-                        ))}
-                    </ul>
-                </div>
-            )}
+            <div>
+                <h1 className="mt-8 mb-4 flex justify-start font-medium">Tareas fijadas:</h1>
+                <ul ref={parent} className="grid grid-cols-1 mt-4 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                    {pinnedTodos.length === 0 ? (
+                        <li>Fija tu primera tarea para comenzar.</li>
+                    ) : (
+                        pinnedTodos.map((todo: any) => (
+                            <li key={todo.id} className="flex flex-col gap-3 w-full bg-cards shadow-xl rounded-lg py-4 px-6">
+                                <div className="flex justify-between items-start truncate">
+                                    <span className={`text-base font-medium ${todo.completed ? 'text-slate-600 line-through' : 'w-full'} transition-all duration-200 ease-in-out truncate`}>
+                                        {todo.title}
+                                    </span>
+                                    {!todo.isPinned ? (
+                                        <button className="material-icons-round scale-75 -mr-1 rotate-45 text-background select-none" onClick={() => togglePin(todo.id)} id={`${todo.id}-pin`}>
+                                            push_pin
+                                        </button>
+                                    ) : (
+                                        <button className="material-icons-round scale-75 -mr-1 rotate-45 select-none" onClick={() => togglePin(todo.id)} id={`${todo.id}-pin`}>
+                                            push_pin
+                                        </button>
+                                    )}
+                                </div>
+                                <div className="flex flex-col items-start truncate">
+                                    <span className={`text-xs w-full truncate ${todo.completed ? 'text-slate-600' : ''} transition-all duration-200 ease-in-out`}>{todo.description}</span>
+                                    <span className={`text-xs w-full truncate ${todo.completed ? 'text-slate-600' : ''} transition-all duration-200 ease-in-out`}>{todo.date}</span>
+                                </div>
+                            </li>
+                        ))
+                    )}
+                </ul>
+            </div>
         </div>
     );
 };
