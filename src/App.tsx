c4b6988c89d9a3f137445c1f8d9d6cfe3d32d306
RafaGonzalezDev <rafagonzalezdeveloper@gmail.com>
2023-08-19 19:32:40 +0200

    Corregido problema fondo de aplicación. Añadida nueva fuente de texto.

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
 
     return (
         <div className="App font-poppins w-full md:max-w-screen-xl h-full md:h-screen p-12">
-            <h1 className="text-2xl text-center text-primarytext font-semibold mb-8">PowerListing</h1>
+            <h1 className="text-3xl text-center text-primarytext font-handlee font-semibold mb-8">PowerListing</h1>
             <TodoPage />
         </div>
     );
